6e0711de7cb0bb3bd16f968ed2cb032e8de2a6c7
quleo <757518614@qq.com>
2015-11-05 16:21:43 +0800

    post data

---
+++ VCACommon/Counter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VCACommon
+{
+    public class Counter
+    {
+        public string Name { get; set; }
+        public DateTime DateTime { get; set; }
+        public int Value { get; set; }
+    }
+}
